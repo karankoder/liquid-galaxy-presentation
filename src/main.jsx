33725e2eda61f00058254b5859d5545a90c234f4
karankoder <karankr.das.cse22@iitbhu.ac.in>
2025-02-20 00:34:38 +0530

    fix minor bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.module.css'
+import './index.css'
 import App from './App.jsx'
 
 export const serverUrl = 'http://localhost:3000/api'
 createRoot(document.getElementById('root')).render(
-  <App />,
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
